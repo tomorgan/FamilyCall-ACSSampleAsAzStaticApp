8ca2e654e9d32b71ca33c965f09806e0bacd09da
Tom Morgan <tom.r.morgan@gmail.com>
2020-09-25 13:26:47 +0100

    sample

---
+++ build/precache-manifest.55510dfa898e254890afa79cd1fa6208.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025694859983183255652b1a8509fd94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1198964e323b6fc11e5",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "0abd3f798aebca795cb4",
+    "url": "/static/js/2.3a284ea1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a284ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1198964e323b6fc11e5",
+    "url": "/static/js/main.c80d5c4e.chunk.js"
+  },
+  {
+    "revision": "9801461823266e21eb50",
+    "url": "/static/js/runtime-main.0575b2c1.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
